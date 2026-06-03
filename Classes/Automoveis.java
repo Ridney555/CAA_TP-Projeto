84f29ee6f991c87276f5c39b85d37b8428ab1a5e
Ridney <ralves.l24@us.edu.cv>
2026-06-03 13:26:25 -0100

    Refresh repository

--- Classes/Automoveis.java
+++ Classes/Automoveis.java
@@ -2,5 +2,37 @@ package Classes;
 public class Automoveis{
     private String modelo;
     private String placa;
-    private double capaciadade;
+    private double capacidade;
+    
+    public Automoveis(String modelo, String placa, double capaciadade){
+        this.modelo = modelo;
+        this.placa = placa;
+        this.capacidade = capaciadade;
+    }
+
+    public String getModelo(){
+        return modelo;
+    }
+
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    public String getPlaca(){
+        return placa;
+    }
+
+    public void setPlaca(String placa){
+        this.placa = placa;
+    }
+
+    public double getCapaciadade(){
+        return capacidade;
+    }
+
+    public void setCapaciadade(double capaciadade){
+        this.capacidade = capaciadade;
+    }
+
+    
 }
--- Classes/Localidades.java
+++ Classes/Localidades.java
@@ -4,11 +4,33 @@ package Classes;
 public class Localidades{
     private int id;
     private String nome;
-
-    public Localidades(int id, String nome) {
+    private double[] cordenadas;  // latitude e longitude
+    
+    public Localidades(int id, String nome, double latitude, double longitude) {
         this.id = id;
         this.nome = nome;
-    } 
+        this.cordenadas = new double[]{latitude, longitude};
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public double[] getCordenadas(){
+        return cordenadas;
+    }
+
+    public double getLatitude(){
+        return cordenadas[0];
+    }
+
+    public double getLongitude(){
+        return cordenadas[1];
+    }
 
     
 }
